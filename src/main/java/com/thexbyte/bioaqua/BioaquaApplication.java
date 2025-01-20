554b5a833b575fe53aaf02e1765c28ac555968bd
abdoutalby <abderrahmentalby@gmail.com>
2025-01-20 22:08:31 +0100

    first commit

---
+++ src/main/java/com/thexbyte/bioaqua/BioaquaApplication.java
@@ -0,0 +1,13 @@
+package com.thexbyte.bioaqua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BioaquaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BioaquaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thexbyte/bioaqua/BioaquaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thexbyte.bioaqua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BioaquaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
