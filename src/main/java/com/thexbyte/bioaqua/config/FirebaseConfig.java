933b19848cf90a81da6a2fdd95e5940afbb0e647
TXBabdoutalby <abderrahmen.talby@thexbyte.com.tn>
2025-03-10 06:28:42 +0100

    Update FirebaseConfig.java

--- src/main/java/com/thexbyte/bioaqua/config/FirebaseConfig.java
+++ src/main/java/com/thexbyte/bioaqua/config/FirebaseConfig.java
@@ -7,16 +7,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void initialize() throws IOException {
-       // FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase-adminsdk.json");
+        // Load the firebase-adminsdk.json file from the classpath
         InputStream serviceAccount = new ClassPathResource("firebase-adminsdk.json").getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
